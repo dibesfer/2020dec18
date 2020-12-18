3c96ca3fb38a71e7003ac43ad46f166abd7139a5
Fer <57419948+dibesfer@users.noreply.github.com>
2020-12-18 16:13:04 +0100

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             Console.WriteLine("Hola cabrón, te voy a enseñar CE SHAR");
+            Console.WriteLine("y además no te vas a enterar de na");
         }
     }
 }
